8e6216a54d68cc4f7ef4aa79d09028db6ba684b6
angela100 <marta.majchrowicz123@gmail.com>
2024-07-29 17:55:49 +0200

    camp part 2

---
+++ src/dzien1_boot2_frontend/src/App.vue
@@ -0,0 +1,39 @@
+<script lang="ts">
+import { ref } from 'vue';
+import { dzien1_boot2_backend } from '../../declarations/dzien1_boot2_backend';
+
+export default {
+  data() {
+    return {
+      newNote: "",
+      notes: [] as string[]
+    }
+  },
+  methods: {
+    async dodajNotatke() {
+      dzien1_boot2_backend.add_note(this.newNote)
+      await this.pobierzNotatki()
+    },
+    async pobierzNotatki() {
+      this.notes = await dzien1_boot2_backend.get_notes()
+    }
+  },
+  mounted() {
+    this.pobierzNotatki()
+  }
+}
+</script>
+
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <br />
+    <br />
+    <div>
+      {{ notes }}
+    </div>
+    <div>
+      <textarea v-model="newNote"><button @click="dodajNotatke">Dodaj notatkę</button></textarea>
+    </div>
+  </main>
+</template>
