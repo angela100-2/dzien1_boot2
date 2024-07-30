3fa2799dc4a9bf80068a578fac0d75cd07ab4a68
angela100 <marta.majchrowicz123@gmail.com>
2024-07-30 16:40:12 +0200

    logowanie bardziej

--- src/dzien1_boot2_frontend/src/App.vue
+++ src/dzien1_boot2_frontend/src/App.vue
@@ -1,9 +1,11 @@
 <script lang="ts">
 import { ref } from 'vue';
-import { dzien1_boot2_backend } from '../../declarations/dzien1_boot2_backend';
+import { createActor, dzien1_boot2_backend } from '../../declarations/dzien1_boot2_backend';
 import { AuthClient } from '@dfinity/auth-client';
 import { HttpAgent } from '@dfinity/agent';
 import type { Identity } from '@dfinity/agent';
+import { Principal } from '@dfinity/principal';
+import { canisterId} from '../../declarations/dzien1_boot2_backend';
 
 export default {
   data() {
@@ -15,21 +17,34 @@ export default {
   },
   methods: {
     async dodajNotatke() {
-      await dzien1_boot2_backend.add_note(this.newNote)
+      if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()) {
+        throw new Error("User not log")
+      }
+      const backend = createActor(canisterId, {
+        agentOptions: {
+          identity: this.identity
+        }
+      })
+      await backend.add_note(this.newNote)
       await this.pobierzNotatki()
     },
+
     async pobierzNotatki() {
-      this.notes = await dzien1_boot2_backend.get_notes(Principal.anonymous)
+      if (!this.identity || this.identity.getPrincipal() === Principal.anonymous()) {
+        throw new Error("User not log")
+      }
+      this.notes = await dzien1_boot2_backend.get_notes(this.identity.getPrincipal())
     },
+
     async login() {
       const authClient = await AuthClient.create()
       await authClient.login({
         identityProvider: "http://avqkn-guaaa-aaaaa-qaaea-cai.localhost:4943/"
       })
+      
       const identity = authClient.getIdentity()
       console.log("Zalogowano", identity.getPrincipal())
       this.identity = identity
-      //const agent = new HttpAgent({ identity })
     }
   },
   mounted(){
